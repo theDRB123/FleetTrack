ab1762fe2b839ae300929fecd3733746abdb8a62
Prit <pritbhanushali012@gmail.com>
2024-01-30 21:08:52 +0530

    modified:   package-lock.json 	modified:   package.json 	new file:   src/AddRoute.js 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/Dashboard.js 	new file:   src/ViewRoutes.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/AddRoute.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+
+const AddRoute = () => {
+  useEffect(() => {
+    const getMap = async () => {
+      const bingKey = "";
+
+      // Dynamically create the Bing Maps script element
+      const script = document.createElement("script");
+      script.src = "https://www.bing.com/api/maps/mapcontrol?callback=GetMap&key=AhP_cuxI2i6AcohWfJLGvOobPxKH11eEfo0TeTDqcQ4PvapLEThf_FQ5OaMgAu-l";
+      document.body.appendChild(script);
+
+      script.onload = () => {
+        // Initialize the map after the Bing Maps script is loaded
+        const map = new window.Microsoft.Maps.Map('#myMap', {});
+
+        // Load the directions module.
+        window.Microsoft.Maps.loadModule('Microsoft.Maps.Directions', () => {
+          // Create an instance of the directions manager.
+          const directionsManager = new window.Microsoft.Maps.Directions.DirectionsManager(map);
+
+          // Specify where to display the route instructions.
+          directionsManager.setRenderOptions({ itineraryContainer: '#directionsItinerary' });
+
+          // Specify where to display the input panel.
+          directionsManager.showInputPanel('directionsPanel');
+        });
+      };
+    };
+
+    // Invoke the function to load the map
+    getMap();
+  }, []); // Empty dependency array to run the effect only once
+
+  return (
+    <div>
+      <div className="directionsContainer">
+        <div id="directionsPanel"></div>
+        <div id="directionsItinerary"></div>
+      </div>
+      <div id="myMap"></div>
+    </div>
+  );
+};
+
+export default AddRoute;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Dashboard from './Dashboard';
+import AddRoute from './AddRoute';
+import ViewRoutes from './ViewRoutes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/AddRoute" element={<AddRoute />} />
+        <Route path="/ViewRoutes" element={<ViewRoutes />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
