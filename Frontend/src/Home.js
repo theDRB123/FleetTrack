ce92b4e661995a2eab3649e1740692c32d93b41a
Somgester <dhruvratmele13@gmail.com>
2024-06-25 18:07:55 +0530

    made some more components

--- Frontend/src/Home.js
+++ Frontend/src/Home.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import Navbar from './Navbar'
+// import Navbar from './Navbar'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
+import SideBar from './components/SideBar'
+import './Home.css'
+// import LoginBtn from './components/LoginBtn'
 
 const Home = () => {
   return (
     <>
-      <Navbar />
-      <Header />
+      {/* <Navbar /> */}
+      <div className="header-sidebar-container">
+        {/* <LoginBtn /> */}
+        <SideBar />
+        <Header />
+      </div>
       <Footer />
     </>
   )
--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import LoginBtn from './LoginBtn';
+import headerImg from '../assets/headerimg.jpg';
 
 const Header = () => {
-    return (
-        <div className='header'>
-            <div className="sidecontent">
-                sidecontents
-            </div>
-            <div className='header-contents'>
-                <h2>Fleet-Track</h2>
-            </div>
-        </div>
-    )
+  return (
+    <div className='header'>
+      <div className="login">
+        <LoginBtn />
+      </div>
+      <img src={headerImg} alt="" />
+      <div className='header-contents'>
+        <h2>Fleet-Track</h2>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
