713859bd438c395740d4f8ace12c4317ed4c25c6
Somgester <dhruvratmele13@gmail.com>
2024-07-17 14:04:22 +0530

    made changes

--- Frontend/src/components/Hamburger.jsx
+++ Frontend/src/components/Hamburger.jsx
@@ -66,10 +66,10 @@ const Hamburger = () => {
               <FontAwesomeIcon icon={'<--'} /> {'close'}
             </button>
             <ul>
-              <li><Link to="/" className="nav-link">Home</Link></li>
-              <li><Link to="/services" className="nav-link">Resources</Link></li>
-              <li><Link to="/contact" className="nav-link">Costumer-Support</Link></li>
-              <li><Link to="/contact" className="nav-link">Complain</Link></li>
+              <li><Link to="/AddRoute" className="nav-link">Add route</Link></li>
+              <li><Link to="/ViewRoutes" className="nav-link">Routes</Link></li>
+              <li><Link to="/VehicleData" className="nav-link">Vehicles</Link></li>
+              <li><Link to="/DriverData" className="nav-link">Drivers</Link></li>
             </ul>
           </div>
         </div>
